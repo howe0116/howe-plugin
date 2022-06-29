02c232936c077709831ba6dcde88a9d890393543
HOWE0116 <howe0116@Gmail.com>
2022-06-29 13:13:28 +0800

    Update help.js

--- apps/help.js
+++ apps/help.js
@@ -7,13 +7,13 @@ const _path = process.cwd();
 
 export const rule = {
   versionInfo: {
-    reg: "^#?howe版本$",
+    reg: "^#?howe参考面板说明$",
     priority: 100,
     describe: "版本",
   }
 };
 
 export function versionInfo(e) {
-  e.reply(`当前版本：v${version}`);
+  e.reply(`参考面板来源:https://bbs.nga.cn/read.php?tid=25843014`);
   return true;
 }
